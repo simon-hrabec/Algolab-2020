322ec91d99d3160e4b5a7c57b388f8ad72355ff2
Šimon Hrabec <simon.hrabec@gmail.com>
2021-01-16 00:42:26 +0100

    add week 08 potw - surveillance photograph

---
+++ Week 08 PotW - Surveillance Photograph/README.md
@@ -0,0 +1,20 @@
+# Surveillance Photograph - solution
+This is a pretty problem. In the description we are told about a network of one way streets and asked to provide maximum number of photographs possible to be delivered with certain conditions met. This is a clear description of a graph and flow problem. However, the condition to use the street at most once when coming back to the station makes it a bit tricky. The trick to use here is a simple graph duplication. We consider the way to the photograph and back independently.
+
+Solution explanation:
+- Duplicating the graph
+- Edges in the graph
+- Max flow
+
+## Duplicating the graph
+After thinking about the problem for a while we see we are having two parts of the problem - the first is a policeman reaching the photograph. Here we have no restriction on how many times the path can be used. However on the way back (to some station, not necessarly the same one) it can be used only once. Hence we create the graph twice, one instance of the graph for each subproblem.
+
+## Edges in the graph
+Having twice the vertices it is now important to correctly decide what edges to build. First we consider the edges representing streets. In the first part of the graph we can give them infinite capacity (pseudo infinity - number of police station will in this case suffice). In the later case we set capacity only to 1 (only a single policeman can use it). We also need a source wertex connected to all police stations in the first graph (capacity 1) and sink/target vertex and having all police stations connected to it (also capacity one). Now we need to think how to connect the two parts. The transition happens when a policeman (or a policewoman?) takes the photograph. Hence, we create an edge of capacity 1 for each photograph node from the first part to the second part.
+
+## Max flow
+After constructing the graph in this manner the maximum flow in the graph is equal to the maximum number of photographs that can be captured with the conditions that only 1 policeman will end up at a police station and the streets taken back to a station will be used just once. Hence, a call to a max flow algorithm `boost::push_relabel_max_flow` will give us the result.
+
+# Running time
+    Test set 1 (40 pts / 1.500 s) - 0.155s
+    Test set 2 (40 pts / 1.500 s) - 0.247s
---
+++ Week 08 PotW - Surveillance Photograph/surveillance_photograph.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+
+using vi = std::vector<int>;
+template<typename T1, typename T2>
+using vp = std::vector<std::pair<T1,T2>>;
+using vpii = vp<int,int>;
+
+template<typename T>
+T load() {
+  T val;
+  std::cin >> val;
+  return val;
+}
+
+template<typename T>
+T loadv(const int count) {
+  T v(count);
+  for(int i = 0; i < count; i++) {
+    std::cin >> v[i];
+  }
+  return v;
+}
+
+template<typename T>
+T loadvp(const int count) {
+  T v(count);
+  for(int i = 0; i < count; i++) {
+    std::cin >> v[i].first;
+    std::cin >> v[i].second;
+  }
+  return v;
+}
+
+using traits = boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS>;
+using graph = boost::adjacency_list<
+  boost::vecS,
+  boost::vecS,
+  boost::directedS,
+  boost::no_property,
+  boost::property<boost::edge_capacity_t, int,
+    boost::property<boost::edge_residual_capacity_t, int,
+      boost::property<boost::edge_reverse_t, traits::edge_descriptor>
+    >
+  >
+>;
+
+using vertex_desc = boost::graph_traits<graph>::vertex_descriptor;
+
+class edge_adder {
+  graph &g;
+
+ public:
+  explicit edge_adder(graph &g) : g(g) {}
+
+  void add_edge(int from, int to, int capacity) {
+    auto c_map = boost::get(boost::edge_capacity, g);
+    auto r_map = boost::get(boost::edge_reverse, g);
+    const auto e = boost::add_edge(from, to, g).first;
+    const auto rev_e = boost::add_edge(to, from, g).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = 0; // reverse edge has no capacity!
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
+  }
+};
+
+void solve(){
+  const auto intersection_count = load<int>();
+  const auto street_count = load<int>();
+  const auto station_count = load<int>();
+  const auto photo_count = load<int>();
+  
+  const auto stations = loadv<vi>(station_count);
+  const auto photos = loadv<vi>(photo_count);
+  const auto streets = loadvp<vpii>(street_count);
+  
+  graph g(2*intersection_count);
+  edge_adder adder(g);
+  const vertex_desc source = boost::add_vertex(g);
+  const vertex_desc target = boost::add_vertex(g);
+  const int offset = intersection_count;
+  
+  // Edges from source
+  // Street to photo
+  // Picking photo - transitioning to a new graph
+  // Streets from photo to station (1 policeman only)
+  // Edges to target
+  
+  for(const auto station : stations) adder.add_edge(source, station, 1);
+  for(const auto street : streets) adder.add_edge(street.first, street.second, station_count);
+  for(const auto photo : photos) adder.add_edge(photo, photo+offset, 1);
+  for(const auto street : streets) adder.add_edge(street.first+offset, street.second+offset, 1);
+  for(const auto station : stations) adder.add_edge(station+offset, target, 1);
+  
+  const int result = boost::push_relabel_max_flow(g, source, target);
+  
+  std::cout << result << std::endl;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  int T;
+  std::cin >> T;
+  while(T--) {
+    solve();
+  }
+}
