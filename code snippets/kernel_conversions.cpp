ad57b975ba30772395d74821ad9f6909e7e148a5
Šimon Hrabec <simon.hrabec@gmail.com>
2021-01-16 12:03:56 +0100

    add code for kernel conversion code snippet

--- code snippets/kernel_conversions.cpp
+++ code snippets/kernel_conversions.cpp
@@ -1,4 +1,8 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
 
-sqrt_points.emplace_back(static_cast<int64_t>(CGAL::exact(iter->x())), static_cast<int64_t>(CGAL::exact(iter->y())));
+using EPECK = CGAL::Exact_predicates_exact_constructions_kernel;
+using EPECK_SQRT = CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt;
+
+EPECK::Point_2 first_point(10, 20);
+EPECK_SQRT::Point_2 second_point(static_cast<int64_t>(CGAL::exact(first_point.x())), static_cast<int64_t>(CGAL::exact(first_point.y())));
