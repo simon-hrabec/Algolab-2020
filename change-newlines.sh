c4265a9f2c6c98472d7abd393ae801c4c5a553c8
Simon hrabec <simon.hrabec@gmail.com>
2021-01-21 21:40:52 +0100

    make newlines script usable with all the tests

--- change-newlines.sh
+++ change-newlines.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-find . -type f \( -name '*.cpp' -o -name 'test_*.in' -o -name 'test_*.out' -o -name 'tags' -o -name '*.sh' -o -name 'README*' -o -name 'Makefile' \) -exec sed -i 's/\x0D$//' {} +
+find . -type f \( -name '*.cpp' -o -name 'test_*.in' -o -name 'test_*.out' -o -name 'tags' -o -name '*.sh' -o -name 'README*' -o -name 'Makefile' \) -print | while read -r filename; do
+	head -1 "$filename" | grep -q $'\r' && sed -i 's/\x0D$//' "$filename"
+done
+exit 0
