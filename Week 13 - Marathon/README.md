6772102b3b93c32577b7499c9ac1b26ff3b13c6a
Šimon Hrabec <simon.hrabec@gmail.com>
2021-02-04 20:06:29 +0100

    Upload better version of week 13 - marathon and update readme

--- Week 13 - Marathon/README.md
+++ Week 13 - Marathon/README.md
@@ -1,32 +1,25 @@
 # Marathon - solution
-This problem is clearly a graph problem. It can be solved by max flow min cost algorithms in some way. The shown solution includes the following steps/techniques:
+This problem is clearly a graph problem. It can be approached in 2 ways (or perhaps more) - we can either apply max-flow-min-cost or use quite elegants and simple solution with max-flow (Thanks Jeniffer for telling me). The shown solution includes the following steps/techniques:
 
 - Dijkstra call to find the shortest path
-- Construction of the flow graph
-- Calling max-flow-min-cost
-- Binary search
+- Restricting the graph
+- Calling max-flow
+- Alternative approach
 
 ## Dijkstra call to find the shortest path
-From the problem description we know the marathon runners will take the shortest (one of it) path from start to finish, therefore a good start is to find the length of the shortest path. We could also find this by using the max-flow-min-cost algorithm by allowing capacity 1 and then checking the cost, however Dijkstra call is cheaper. The only thing we pay is more code and 2x graph definition (which is quite verbose for Boost Graph Library). 
+From the problem description we know the marathon runners will take the shortest (one of it) path from start to finish, therefore a good start is to find the length of the shortest path. Note that by this we find the distance and we might be also able to find one of the paths, however there might be multiple (exponentially many) paths of the minimal/optimal length.
 
-## Construction of the flow graph
-We take all the given edges and create 2 directed edges for each undirected edge with the specified capacity and cost being the length. Additionally we add one more edge to the start vertex with 0 cost and capacity we want to try out (see the next point).
+## Restricting the graph
+The main idea to this solution is that the runners (in order to achieve the minimal legth) will run only on the shortest paths. Hence, we can remove (set capacity 0) to paths that are not on these paths. The smart way to find those edges is to run dijsktra one more time - in this case from finish. For each edge, we can then say what is the distance from start to starting vertex of the edge and from the ending vertex of the edge to finish (since the graph is undirected). Putting the pieces together - if the edge length + distance from start + distance from finish equals the optimal distance then this edge is on some shortest path and should be preserved. Otherwise it can be eliminated (capacity set to 0).
 
-## Calling max-flow-min-cost
-We now know the length of the shortest path from start to finish. Each 1 distance unit ran should result in cost 1. Therefore if N runners run K km race the cost should be N*K if they all chose the optimal route (any). However, if the cost is higher it means some runner had to chose a longer path and hence N is not achievable.
+## Calling max-flow
+Having the graph transformed in the described manner the result can then be computed via a simple `boost::push_relabel_max_flow` call. The result can be therefore obtained by 2 calls to dijsktra, iterating over the edges and a single call to max-flow, which all considerably fast.
 
-## Binary search
-Having a way to check if the city can accommodate N runners we just do a binary search to find the highest possible value. There are several variations of binary search with small differences in the update rule. In this specific case we have two options:
-1. `Cost == Expected cost` - This is a possible result. We discard all smaller instances and set `low=mid`.
-2. `Cost > Expected cost` - This N is impossible and so are all bigger N so we set `high=mid-1`.
-
-Last thing to be careful in binary search implementation is to avoid an infinite loop. If we have last 2 elements `mid` will be set to either `high` or `low`. Since we update `low=mid` we need to make sure to have `mid` equal to `high` in case there are last 2 elements in the range. `(low+high+1)/2` Simple +1 and integer division will do the work, no need for floating numbers and rounding/ceiling.
-
-# Additional notes
-I tried using lambdas with wide capture heavily in this code. The graphs could be encapsulated into separate classes but using the lambda approach you can read the whole code from line 0 without jumping up and down (위아레) and still avoid code reuse. Also, I do not recreate the graph but only rewrite the capacity values, which is the only part that change.
+## Alternative approach
+Another way (quite suboptimal, slower - yet interesting to mention) to obtain the result is to use dijkstra to calculate the length of the shortest path and then use the max-flow-min-cost with binary search to find what is the maxium of people to run without using a longer path. We need to use one edge at the start to set the flow and then in each iteration of the binary search compare the cost with the expected cost, which is `flow * min_distance`. It can be interesting for practice of max-flow-min-cost problems to code this up, but keep in mind this problem has much faster and shorter solution. You can also just inspect the code as in this case I tried using lambdas with wide capture heavily in this code. The graphs could be encapsulated into separate classes but using the lambda approach you can read the whole code from line 0 without jumping up and down (위아레) and still avoid code reuse. Also, I do not recreate the graph but only rewrite the capacity values, which is the only part that change.
 
 # Running time
-    Test set 1 (25 pts / 1 s) - 0.488s
-    Test set 2 (25 pts / 1 s) - 0.09s
-    Test set 3 (25 pts / 1 s) - 0.116s
-    Test set 4 (25 pts / 1 s) - 0.7s
+    Test set 1 (25 pts / 1 s) - 0.024s
+    Test set 2 (25 pts / 1 s) - 0.029s
+    Test set 3 (25 pts / 1 s) - 0.01s
+    Test set 4 (25 pts / 1 s) - 0.032s
--- Week 13 - Marathon/marathon.cpp
+++ Week 13 - Marathon/marathon.cpp
@@ -4,29 +4,34 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <boost/graph/push_relabel_max_flow.hpp>
-#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
-#include <boost/graph/find_flow_cost.hpp>
 
-namespace distance{
-  using graph = boost::adjacency_list< 
-    boost::vecS, 
-    boost::vecS, 
-    boost::undirectedS,
-    boost::no_property,
-    boost::property<boost::edge_weight_t, int>
-  >;
-  using weight_map = boost::property_map<graph, boost::edge_weight_t>::type;  
-}
+using traits = boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS>;
+using graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, int64_t,
+    boost::property<boost::edge_residual_capacity_t, int64_t,
+    boost::property<boost::edge_reverse_t, traits::edge_descriptor,
+    boost::property <boost::edge_weight_t, int64_t>>>>
+>;
 
-namespace flow {
-  using traits = boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS>;
-  using graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
-      boost::property<boost::edge_capacity_t, int64_t,
-      boost::property<boost::edge_residual_capacity_t, int64_t,
-      boost::property<boost::edge_reverse_t, traits::edge_descriptor,
-      boost::property <boost::edge_weight_t, int64_t>>>>
-  >;
-}
+class edge_adder {
+ graph &G;
+
+ public:
+  explicit edge_adder(graph &G) : G(G) {}
+  void add_edge(int from, int to, int64_t capacity, int64_t cost) {
+    auto c_map = boost::get(boost::edge_capacity, G);
+    auto r_map = boost::get(boost::edge_reverse, G);
+    auto w_map = boost::get(boost::edge_weight, G);
+    const auto e = boost::add_edge(from, to, G).first;
+    const auto rev_e = boost::add_edge(to, from, G).first;
+    c_map[e] = capacity;
+    c_map[rev_e] = capacity;
+    r_map[e] = rev_e;
+    r_map[rev_e] = e;
+    w_map[e] = cost;
+    w_map[rev_e] = cost; 
+  }
+};
 
 template <typename T>
 T load(){
@@ -35,89 +40,62 @@ T load(){
   return val;
 }
 
+template <typename T>
+struct iter_wrapper{
+  T iter, iter_end;
+  T begin() const {return iter;}
+  T end() const {return iter_end;}
+};
+
+template <typename T>
+iter_wrapper<typename T::first_type> range_loop_wrapper(const T& iter_pair) {
+  return iter_wrapper<typename T::first_type>{iter_pair.first, iter_pair.second};
+}
+
+void dijsktra_wrapper(const graph &G, std::vector<int64_t> &distance_map, const int start_vertex) {
+  boost::dijkstra_shortest_paths(G, start_vertex, boost::distance_map(
+      boost::make_iterator_property_map(distance_map.begin(), boost::get(boost::vertex_index,G)))
+  );
+}
+
 void solve() {
-    const int nr_cities = load<int>();
-    const int nr_streets = load<int>();
-    const int start = load<int>();
-    const int finish = load<int>();
-
-    // Graph definition (for min distance)
-    distance::graph DG(nr_cities);
-    distance::weight_map distance_weights = boost::get(boost::edge_weight, DG);
-    const auto add_edge_to_distance_graph = [&DG, distance_weights](const int from, const int to, const int distance){
-      const auto edge = boost::add_edge(from, to, DG).first;
-      distance_weights[edge] = distance;
-    };
-
-    // Graph definition (for flow cost binary search)
-    flow::graph FG(nr_cities);
-    const auto source = boost::add_vertex(FG);
-    auto flow_c_map = boost::get(boost::edge_capacity, FG);
-    auto flow_r_map = boost::get(boost::edge_reverse, FG);
-    auto flow_w_map = boost::get(boost::edge_weight, FG); // new!
-    const auto add_edge_to_flow_graph = [&flow_c_map, &flow_r_map, &flow_w_map, &FG](const int from, const int to, const int capacity, const int cost){
-      const auto edge = boost::add_edge(from, to, FG).first;
-      const auto rev_edge = boost::add_edge(to, from, FG).first;
-      flow_c_map[edge] = capacity;
-      flow_c_map[rev_edge] = 0;
-      flow_r_map[edge] = rev_edge;
-      flow_r_map[rev_edge] = edge;
-      flow_w_map[edge] = cost;
-      flow_w_map[rev_edge] = -cost;
-    };
+    const auto nr_cities = load<int>();
+    const auto nr_streets = load<int>();
+    const auto start = load<int>();
+    const auto finish = load<int>();
+    
+    graph G(nr_cities);
+    edge_adder adder(G);
+    auto capacity_map = boost::get(boost::edge_capacity, G);
+    auto weight_map = boost::get(boost::edge_weight, G);
 
     // Load edges and add them to graphs
     for(int i = 0; i < nr_streets; i++) {
-      const int from = load<int>();
-      const int to = load<int>();
-      const int capacity = load<int>();
-      const int distance = load<int>();
+      const auto from = load<int>();
+      const auto to = load<int>();
+      const auto capacity = load<int>();
+      const auto distance = load<int>();
 
-      add_edge_to_distance_graph(from, to, distance);
-      add_edge_to_flow_graph(from, to, capacity, distance);
-      add_edge_to_flow_graph(to, from, capacity, distance);
+      adder.add_edge(from, to, capacity, distance);
     }
 
-    // Add special edge to regulate max capacity
-    const auto source_edge = boost::add_edge(source, start, FG).first;
-    const auto source_rev_edge = boost::add_edge(start, source, FG).first;
-    flow_c_map[source_edge] = std::numeric_limits<int>::max();
-    flow_c_map[source_rev_edge] = 0;
-    flow_r_map[source_edge] = source_rev_edge;
-    flow_r_map[source_rev_edge] = source_edge;
-    flow_w_map[source_edge] = 0;
-    flow_w_map[source_rev_edge] = 0;
+    // Run dijstra twice to find distances from both start and finish
+    std::vector<int64_t> distance_from_start(nr_cities), distance_from_finish(nr_cities);
+    dijsktra_wrapper(G, distance_from_start, start);
+    dijsktra_wrapper(G, distance_from_finish, finish);
+    const int64_t min_distance = distance_from_start[finish];
 
-    // Save set capacity
-    const auto flow_c_map_backup = flow_c_map;
-    const auto set_graph_to_max_cap = [source_edge, &flow_c_map, &flow_c_map_backup](const int capacity){
-      flow_c_map = flow_c_map_backup;
-      flow_c_map[source_edge] = capacity;
-    };
-
-    // Run dijkstra to find the shortest path
-    std::vector<int> dist_map(nr_cities);
-    boost::dijkstra_shortest_paths(DG, start,
-      boost::distance_map(boost::make_iterator_property_map(dist_map.begin(), boost::get(boost::vertex_index, DG))));
-    const int total_distance = dist_map[finish];
-
-    // Run binary search to find biggest possible number of participants
-    const long max_capacity = boost::push_relabel_max_flow(FG, source, finish);
-    long low = 1, high = max_capacity;
-    while(low != high) {
-      const long mid = (low+high+1)/2;
-      const long expected_cost = total_distance * mid;
-      set_graph_to_max_cap(mid);
-      boost::successive_shortest_path_nonnegative_weights(FG, source, finish);
-      const long cost = boost::find_flow_cost(FG);
-      if (cost == expected_cost) {
-        low = mid;
-      } else {
-        high = mid-1;
+    // Discard edges that are not on the shortest path (paths)
+    for(const auto &edge : range_loop_wrapper(boost::edges(G))) {
+      const auto from = boost::source(edge, G);
+      const auto to  = boost::target(edge, G);
+      if (weight_map[edge] + distance_from_start[from] + distance_from_finish[to] != min_distance) {
+        capacity_map[edge] = 0;
       }
     }
 
-    std::cout << low << std::endl;
+    const int64_t result = boost::push_relabel_max_flow(G, start, finish);
+    std::cout << result << std::endl;
 }
 
 int main() {
