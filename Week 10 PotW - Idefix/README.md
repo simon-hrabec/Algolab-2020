5c7364f5e99cbdec38c4b0330c9ecb2a77a4ee65
Šimon Hrabec <simon.hrabec@gmail.com>
2021-07-08 23:34:10 +0200

    add week 10 potw - idefix

---
+++ Week 10 PotW - Idefix/README.md
@@ -0,0 +1,31 @@
+# Idéfix
+Idéfix is truly a fun problem to solve. It has moderately difficult description, it contains geometry, requires use of multiple data structures and is not tricky in terms of coding. Moreover the solution is somehow straightforward. The problem is somehow similar to "Hand" from week 11.
+- Input format 
+- Problem classification
+- Q1 - Maximum reachable bones
+- Q2 - Smallest possible radius
+- Implementation notes
+
+## Input format
+An important thing to notice is that the problem description talks about radius but we are given **s=4\*r^2**, which is much more convenient. Calculating distance between 2 points requires square root, but s in the given form allows us to always work with squared distances. Since both x and y coordinates are integers the squared distance will also be an integer.
+
+## Problem classification
+The problem talks about points (trees) and their surroundings (discs) and bones and their distance to the trees. This is quite straightforward call to use `CGAL::Delaunay_triangulation_2` which allows us to find the closest point (tree) to any random point (bone). It also gives us the triangulation edges that we can use together with union-find.
+
+## Q1 - Maximum reachable bones
+The first questions we are tasked to answer is if we join the are of the disc (determined by the points and r or s) how many bones will contain the segment with the most bones. In this case we are told s (4\*r^2) and the k (number of bones) is unknown. It sounds logical to join the trees into groups that create connected area (continuous shadow) and then iterate over the bones and check if the bone falls into any of the segments and if so count the occurrences and then return the maximum.
+
+To get the continuous segments we can use a union-find data structure (`boost::disjoint_sets_with_storage<>`) and iterate over all edges from the delunay triangulation and join the segments if the two points are sufficiently close. Then we set up counters for each segment and iterate over the bones and using `CGAL::Delaunay_triangulation_2::nearest_vertex` we get a vertex that falls into some segment and if is sufficiently close we increment the counter. The maximum of these counters is the answer we are looking for
+
+## Q2 - Smallest possible radius
+Now we know the number of bones k but we need to find s instead. For this case we are going to use union-find again. It sounds logical to take the edges and sort them, then iterate over them and join the segments until we have enough bones and return the (squared) length of the last edge required. However the catch here is that we have no way to how many bones currently are in the segments. Also, extending the radius just to include an additional bone can be enough to reach k. This asks to somehow include bones in the union-find merging process. The idea is to create and edge for each bone to its closest tree. This poses a slight issue that we now have 2 types of edges that we have to treat differently, but on the other hand we can sort them the same way just by the distance. After that we process the edges and do the following depending on the type:
+- Edge between 2 trees - If the 2 trees belong to 2 different segments then join the segments and add their bone count. If the sum reaches K return the length of the current edge
+- Edge from a bone to a tree - Increment the bone count of the target segment by 1. If the count reaches K return the length of the edge (squared distance from the bone to its closes tree).
+
+## Implementation notes
+The triangulation need to use vertices with indices  `CGAL::Triangulation_vertex_base_with_info_2<std::size_t,EPIC_KERNEL>` to be able to reference them when iterating over the triangulation edges or when iterating over bones and checking the closest trees.
+
+# Running time
+    Test set 1 (20 pts / 1 s) - 0.016s
+    Test set 2 (20 pts / 1 s) - 0.086s
+    Test set 3 (20 pts / 1 s) - 0.027s
---
+++ Week 10 PotW - Idefix/idefix.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <CGAL/Triangulation_face_base_2.h>
+
+#include <boost/pending/disjoint_sets.hpp>
+
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Index = std::size_t;
+using Vb = CGAL::Triangulation_vertex_base_with_info_2<Index,K>;
+using Fb = CGAL::Triangulation_face_base_2<K>;
+using Tds = CGAL::Triangulation_data_structure_2<Vb,Fb>;
+using Triangulation = CGAL::Delaunay_triangulation_2<K,Tds>;
+using Point = K::Point_2;
+using IPoint = std::pair<K::Point_2, Index>;
+
+// if bool = false - idx1, idx1, distance
+// if bool = true - tree_idx, tree_idx, distance
+using universal_edge = std::tuple<Index, Index, K::FT, bool>;
+
+template<typename T>
+T load() {
+  T val;
+  std::cin >> val;
+  return val;
+}
+
+Point loadp() {
+  int x, y;
+  std::cin >> x >> y;
+  return Point{x, y};
+}
+
+void solve() {
+  // Load data, process trees/bones, define used data structures
+  const std::size_t tree_count = load<int>();
+  const std::size_t bone_count = load<int>();
+  const double sradius = load<double>();
+  const int more_bones_count = load<int>();
+ 
+  std::vector<IPoint> trees;
+  std::vector<int> tree_has_bones(tree_count); 
+  std::vector<universal_edge> edges;
+  boost::disjoint_sets_with_storage<> uf(tree_count);
+  
+  trees.reserve(tree_count);
+  for(Index i = 0; i < tree_count; i++) {
+    const Point tree = loadp();
+    trees.emplace_back(tree, i);
+  }
+  
+  Triangulation t(std::begin(trees), std::end(trees));
+  
+  // Create bone edges and calculate bone count for trees
+  for(Index i = 0; i < bone_count; i++) {
+    const Point bone = loadp();
+    
+    Index idx = t.nearest_vertex(bone)->info();
+    const double dis = 4*CGAL::squared_distance(bone, trees[idx].first);
+    edges.emplace_back(idx, idx, dis, true);
+    if (dis <= sradius) {
+      tree_has_bones[idx]++;
+    }
+  }
+
+  // Iterate over all triangulation edges, connect components
+  for (auto e = t.finite_edges_begin(); e != t.finite_edges_end(); ++e) {
+    Index i1 = e->first->vertex((e->second+1)%3)->info();
+    Index i2 = e->first->vertex((e->second+2)%3)->info();
+    
+    if (i1 > i2) {
+      std::swap(i1, i2);
+    }
+    
+    const auto &p1 = trees[i1].first;
+    const auto &p2 = trees[i2].first;
+    
+    const double dis = CGAL::squared_distance(p1, p2);
+    
+    edges.emplace_back(i1, i2, dis, false);
+    
+    if (dis <= sradius) {
+      Index c1 = uf.find_set(i1);
+      Index c2 = uf.find_set(i2);
+      
+      if (c1 != c2) {
+        uf.link(c1, c2);
+        tree_has_bones[uf.find_set(i1)] = tree_has_bones[c1] + tree_has_bones[c2];
+      }
+    }
+  }
+  
+  const int max_reachable_bones = *max_element(std::begin(tree_has_bones), std::end(tree_has_bones));
+  uint64_t min_required_radius = 0;
+  
+  std::sort(std::begin(edges), std::end(edges), [](const universal_edge& e1, const universal_edge& e2) {
+    return std::get<2>(e1) < std::get<2>(e2);
+  });
+  
+  uf = boost::disjoint_sets_with_storage<>(tree_count);
+  std::fill(std::begin(tree_has_bones), std::end(tree_has_bones), 0);
+  
+  // Iterate from shortest edges to find minimal required radius
+  for(const universal_edge &e : edges) {
+    const Index i1 = std::get<0>(e);
+    const Index i2 = std::get<1>(e);
+    const double dis = std::get<2>(e);
+    const bool is_bone = std::get<3>(e);
+    
+    if (is_bone) {
+      Index c1 = uf.find_set(i1);
+      tree_has_bones[c1]++;
+      if (tree_has_bones[c1] >= more_bones_count) {
+        min_required_radius = dis;
+        break;
+      }
+    } else {
+      Index c1 = uf.find_set(i1);
+      Index c2 = uf.find_set(i2);
+      
+      if (c1 != c2) {
+        uf.link(c1, c2);
+        Index cnew = uf.find_set(i1);
+        tree_has_bones[cnew] = tree_has_bones[c1] + tree_has_bones[c2];
+        
+        if (tree_has_bones[cnew] >= more_bones_count) {
+          min_required_radius = dis;
+          break;
+        }
+      }
+    }
+  }
+  
+  std::cout << max_reachable_bones << " " << min_required_radius << std::endl;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  int T; std::cin >> T;
+  while(T--) {
+    solve();
+  }
+}
