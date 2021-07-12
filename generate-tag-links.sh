ef7f2f59bdce8e2dca55f044d4601fa1a6c8eb59
Šimon Hrabec <simon.hrabec@gmail.com>
2021-07-12 02:35:12 +0200

    fix issue with readme tags script

--- generate-tag-links.sh
+++ generate-tag-links.sh
@@ -6,10 +6,12 @@
 
 REPO_PATH="${1:-https://github.com/simon-hrabec/Algolab2020/tree/main/problems/}"
 
+cd problems/
+
 echo "## Techniques/algorithms/data structures"
 find . -type f -name "tags" -exec cat {} \; | sort | uniq | while read -r tag; do
 	echo -n "- ${tag} -"
-	grep -rl "$tag" problems/*/tags | while read -r problem; do
+	grep -rl "$tag" */tags | while read -r problem; do
 		echo -n " [W$(echo "$problem" | sed 's/[^0-9]*//g' )/$(echo "$problem" | sed 's|.* - \(.*\)/tags|\1|g')]"
 		echo -n "($REPO_PATH$(echo "$problem" | sed 's|/tags||g' | sed 's| |%20|g'))"
 	done
